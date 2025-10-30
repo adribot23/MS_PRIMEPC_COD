0e9e0a0d2392eaef8a8c021bf2b94b6a9ff5c7f2
Mateo Gutierrez <mateogut@ucm.es>
2025-10-30 13:35:31 +0100

    VBuscarAlmacen

--- PrimePcCode/src/presentacion/Almacen/VBuscarAlmacen.java
+++ PrimePcCode/src/presentacion/Almacen/VBuscarAlmacen.java
@@ -3,11 +3,20 @@
  */
 package presentacion.Almacen;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import presentacion.Controller.Controlador;
 import presentacion.Controller.Command.Context;
 import presentacion.GUI.IGUI;
+import presentacion.GUI.Evento;
 
 /** 
 * <!-- begin-UML-doc -->
@@ -29,11 +38,25 @@ public class VBuscarAlmacen extends JPanel implements IGUI {
 	* @return
 	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
-	public Void initGUI() {
-		// begin-user-code
-		// TODO Ap�ndice de m�todo generado autom�ticamente
-		return null;
-		// end-user-code
+	public void initGUI() {
+		setBorder(BorderFactory.createTitledBorder("Buscar Almacen"));
+		setLayout(new GridLayout(3, 1, 5, 5));
+
+		JTextField idField = new JTextField();
+		JButton buscarButton = new JButton("Buscar");
+		buscarButton.setBackground(new Color(200, 255, 200));
+		add(new JLabel("ID almacen:"));
+		add(idField);
+		add(buscarButton);
+
+		buscarButton.addActionListener(e -> {
+			try {
+				int id = Integer.parseInt(idField.getText().trim());
+				Controlador.getInstancia().accion(new Context(Evento.BUSCAR_ALMACEN, id));
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(this, "El ID debe ser un numero.");
+			}
+		});
 	}
 
 	public void actualizar(Context context) {
