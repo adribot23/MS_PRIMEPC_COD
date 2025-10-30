d2f02d2fce93ed9e1769f5b241c811a0365f705b
Mateo Gutierrez <mateogut@ucm.es>
2025-10-30 12:58:00 +0100

    VVerPorProveedor

--- PrimePcCode/src/presentacion/Producto/VVerProdPorProveedor.java
+++ PrimePcCode/src/presentacion/Producto/VVerProdPorProveedor.java
@@ -3,11 +3,19 @@
  */
 package presentacion.Producto;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import presentacion.Controller.Controlador;
 import presentacion.Controller.Command.Context;
 import presentacion.GUI.IGUI;
+import presentacion.GUI.Evento;
 
 /** 
 * <!-- begin-UML-doc -->
@@ -29,11 +37,24 @@ public class VVerProdPorProveedor extends JPanel implements IGUI {
 	* @return
 	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
-	public Void initGUI() {
-		// begin-user-code
-		// TODO Ap�ndice de m�todo generado autom�ticamente
-		return null;
-		// end-user-code
+	public void initGUI() {
+		setLayout(new GridLayout(1, 2));
+		setBorder(BorderFactory.createTitledBorder("Productos por Proveedor"));
+
+		JTextField txtIdProveedor = new JTextField();
+		JButton btnPorProveedor = new JButton("Mostrar por Proveedor");
+		btnPorProveedor.setBackground(new Color(200, 255, 200));
+		add(txtIdProveedor);
+		add(btnPorProveedor);
+
+		btnPorProveedor.addActionListener(e -> {
+			try {
+				int idProveedor = Integer.parseInt(txtIdProveedor.getText());
+				ctrl.accion(new Context(Evento.MOSTRAR_PRODUCTOS_POR_PROVEEDOR, idProveedor));
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(this, "ID erroneo.");
+			}
+		});
 	}
 
 	public void actualizar(Context context) {
