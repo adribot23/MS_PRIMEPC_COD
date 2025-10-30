6e640390efcf10a5bd200faac468dc677c1dadc4
Mateo Gutierrez <mateogut@ucm.es>
2025-10-30 13:06:32 +0100

    GUIPRoveedor(parte2)

--- PrimePcCode/src/presentacion/Proveedor/GUIProveedor.java
+++ PrimePcCode/src/presentacion/Proveedor/GUIProveedor.java
@@ -8,14 +8,19 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.Collection;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.SwingConstants;
 
+import negocio.Proveedor.TProveedor;
 import presentacion.Controller.Command.Context;
 import presentacion.GUI.IGUI;
 import presentacion.Proveedor.VAltaProveedor;
@@ -25,6 +30,7 @@ import presentacion.Proveedor.VModificarProveedor;
 import presentacion.Proveedor.VMostrarProveedor;
 import presentacion.Proveedor.VVerPorProducto;
 import presentacion.Proveedor.VVincularProveedor;
+import presentacion.GUI.Evento;
 
 /** 
 * <!-- begin-UML-doc -->
@@ -33,7 +39,7 @@ import presentacion.Proveedor.VVincularProveedor;
 * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 */
 public class GUIProveedor extends JPanel implements IGUI {
-	public void actualizar(Context context) {
+	public void initGUI(Context context) {
 		setLayout(new BorderLayout());
 
 		JLabel titulo = new JLabel("MODULO PROVEEDOR", SwingConstants.CENTER);
@@ -82,5 +88,86 @@ public class GUIProveedor extends JPanel implements IGUI {
 		});
 
 		add(btnVolver, BorderLayout.SOUTH);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void actualizar(Context context) {
+		Evento evento = context.getEvento();
+		Object datos = context.getDatos();
+		switch (evento) {
+		case RES_ALTA_PROVEEDOR_OK:
+			JOptionPane.showMessageDialog(null, "Proveedor dado de alta con ID: " + datos);
+			break;
+		case RES_ALTA_PROVEEDOR_KO:
+			JOptionPane.showMessageDialog(null, "Error al dar de alta el proveedor.");
+			break;
+		case RES_BAJA_PROVEEDOR_OK:
+			JOptionPane.showMessageDialog(null, "Proveedor dado de baja correctamente.");
+			break;
+		case RES_BAJA_PROVEEDOR_KO:
+			JOptionPane.showMessageDialog(null, "Error al dar de baja el proveedor.");
+			break;
+		case RES_MODIFICAR_PROVEEDOR_OK:
+			JOptionPane.showMessageDialog(null, "Proveedor modificado correctamente.");
+			break;
+		case RES_MODIFICAR_PROVEEDOR_KO:
+			JOptionPane.showMessageDialog(null, "Error al modificar proveedor.");
+			break;
+		case RES_BUSCAR_PROVEEDOR_OK:
+			JOptionPane.showMessageDialog(null, datos.toString());
+			break;
+		case RES_BUSCAR_PROVEEDOR_KO:
+			JOptionPane.showMessageDialog(null, "Proveedor no encontrado.");
+			break;
+		case RES_MOSTRAR_TODOS_PROVEEDORES_OK:
+			mostrarTabla((Collection<TProveedor>) datos);
+			break;
+		case RES_MOSTRAR_TODOS_PROVEEDORES_KO:
+			JOptionPane.showMessageDialog(null, "No hay proveedores para mostrar.");
+			break;
+		case RES_MOSTRAR_PROVEEDORES_POR_PRODUCTO_OK:
+			JOptionPane.showMessageDialog(null, "Producto suministrado por " + (TProveedor) datos);
+			break;
+		case RES_MOSTRAR_PROVEEDORES_POR_PRODUCTO_KO:
+			JOptionPane.showMessageDialog(null, "No se encontro proveedor para ese producto.");
+			break;
+		case RES_VINCULAR_PRODUCTO_PROVEEDOR_OK:
+			JOptionPane.showMessageDialog(null, "Producto vinculado al proveedor correctamente.");
+			break;
+		case RES_VINCULAR_PRODUCTO_PROVEEDOR_KO:
+			JOptionPane.showMessageDialog(null, "No se pudo vincular el producto al proveedor.");
+			break;
+		case RES_DESVINCULAR_PRODUCTO_PROVEEDOR_OK:
+			JOptionPane.showMessageDialog(null, "Producto desvinculado al proveedor correctamente.");
+			break;
+		case RES_DESVINCULAR_PRODUCTO_PROVEEDOR_KO:
+			JOptionPane.showMessageDialog(null, "No se pudo desvincular el producto del proveedor.");
+			break;
+		default:
+			JOptionPane.showMessageDialog(null, "Evento no reconocido: " + evento);
+		}
+	}
+
+	private void mostrarTabla(Collection<TProveedor> proveedores) {
+
+		String[] columnNames = { "ID", "Nombre", "Activo" };
+		Object[][] tableData = new Object[proveedores.size()][columnNames.length];
+
+		int i = 0;
+		for (TProveedor p : proveedores) {
+			tableData[i][0] = p.getId();
+			tableData[i][1] = p.getNombre();
+			tableData[i][2] = p.getActivo();
+			i++;
+		}
+
+		JTable table = new JTable(tableData, columnNames);
+		table.setFillsViewportHeight(true);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		table.setEnabled(false);
+		JScrollPane scrollPane = new JScrollPane(table);
+		JOptionPane.showMessageDialog(null, scrollPane, "Proveedores", JOptionPane.PLAIN_MESSAGE);
+
 	}
 }
