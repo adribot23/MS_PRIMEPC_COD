bdf25f08202895986701128d79bf6a16380ee02a
Mateo Gutierrez <mateogut@ucm.es>
2025-10-30 12:26:17 +0100

    VCalcularImporteMasVendido

--- PrimePcCode/src/presentacion/Empleado/VCalcularImporteMasVendido.java
+++ PrimePcCode/src/presentacion/Empleado/VCalcularImporteMasVendido.java
@@ -3,7 +3,19 @@
  */
 package presentacion.Empleado;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import presentacion.Controller.Controlador;
+import presentacion.Controller.Command.Context;
+import presentacion.GUI.Evento;
 
 /** 
 * <!-- begin-UML-doc -->
@@ -11,7 +23,7 @@ import presentacion.Controller.Controlador;
 * @author adria
 * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 */
-public class VCalcularImporteMasVendido {
+public class VCalcularImporteMasVendido extends JPanel{
 	/** 
 	* <!-- begin-UML-doc -->
 	* <!-- end-UML-doc -->
@@ -25,9 +37,23 @@ public class VCalcularImporteMasVendido {
 	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
 	public void initGUI() {
-		// begin-user-code
-		// TODO Ap�ndice de m�todo generado autom�ticamente
+		setLayout(new GridLayout(3, 1));
+		setBorder(BorderFactory.createTitledBorder("Calcular Importe Más Vendido"));
 
-		// end-user-code
+		JTextField txtBuscarID = new JTextField();
+		JButton btnBuscar = new JButton("Calcular");
+		btnBuscar.setBackground(new Color(200, 255, 200));
+		add(new JLabel("ID producto:"));
+		add(txtBuscarID);
+		add(btnBuscar);
+
+		btnBuscar.addActionListener(e -> {
+			try {
+				int id = Integer.parseInt(txtBuscarID.getText());
+				ctrl.accion(new Context(Evento.CALCULAR_MAS_VENDIDO, id));
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(this, "ID debe ser un numero.");
+			}
+		});
 	}
 }
