fb8b25e7f1ce5f250a0de644a9663df5ec47e151
AlvaroAdrada26 <aadrada@ucm.es>
2025-11-01 13:23:17 +0100

    VMostrarCliente arreglado

--- PrimePcCode/src/presentacion/Cliente/VMostrarCliente.java
+++ PrimePcCode/src/presentacion/Cliente/VMostrarCliente.java
@@ -40,7 +40,7 @@ public class VMostrarCliente extends JPanel {
 		JButton btnMostrarTodos = new JButton("Mostrar todos los clientes");
 		btnMostrarTodos.setBackground(new Color(200, 255, 200));
 		btnMostrarTodos.addActionListener(e -> {
-			Controlador.getInstancia().accion(new Context(Evento.MOSTRAR_TODOS_EMPLEADOS, null));
+			Controlador.getInstancia().accion(new Context(Evento.MOSTRAR_TODOS_CLIENTES, null));
 		});
 
 		add(btnMostrarTodos);
