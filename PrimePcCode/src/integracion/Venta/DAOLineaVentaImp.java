0842a2f23444bec09822144c21fdda7de692eda4
Daritessar <dariorom@ucm.es>
2025-11-03 21:14:39 +0100

    Añadido conexion a la base de datos, añadido tabla lineaVenta

--- PrimePcCode/src/integracion/Venta/DAOLineaVentaImp.java
+++ PrimePcCode/src/integracion/Venta/DAOLineaVentaImp.java
@@ -1,4 +1,3 @@
-
 package integracion.Venta;
 
 import java.sql.Connection;
@@ -19,12 +18,11 @@ public class DAOLineaVentaImp implements DAOLineaVenta {
 			Transaction tr = tm.getTransaction();
 			Connection con = (Connection)tr.getResource();
 			
-			PreparedStatement ps = con.prepareStatement("INSERT INTO LINEAVENTA (id_producto, id_venta, num_unidades, precio_unidades, activo) VALUES (?, ?, ?, ?, ?)");
+			PreparedStatement ps = con.prepareStatement("INSERT INTO LINEAVENTA (ID_PRODUCTO, ID_VENTA, NUM_UNIDADES, PRECIO_UNIDADES) VALUES (?, ?, ?, ?)");
 			ps.setInt(1, lineaVenta.get_producto());
 			ps.setInt(2, lineaVenta.get_venta());
 			ps.setInt(3, lineaVenta.get_num_unidades());
 			ps.setDouble(4, lineaVenta.get_precio_unidades());
-			ps.setInt(5, lineaVenta.get_activo());
 			
 			int filas = ps.executeUpdate();
 			ps.close();
@@ -48,7 +46,7 @@ public class DAOLineaVentaImp implements DAOLineaVenta {
 			Transaction tr = tm.getTransaction();
 			Connection con = (Connection)tr.getResource();
 			
-			PreparedStatement ps = con.prepareStatement("UPDATE LINEAVENTA SET activo = 0 WHERE id_producto = ? AND id_venta = ?");
+			PreparedStatement ps = con.prepareStatement("DELETE LINEAVENTA WHERE ID_PRODUCTO = ? AND ID_VENTA = ?");
 			ps.setInt(1, tlineaVenta.get_producto());
 			ps.setInt(2, tlineaVenta.get_venta());
 			
@@ -74,7 +72,7 @@ public class DAOLineaVentaImp implements DAOLineaVenta {
 			Transaction tr = tm.getTransaction();
 			Connection con = (Connection)tr.getResource();
 			
-			PreparedStatement ps = con.prepareStatement("SELECT id_producto, id_venta, num_unidades, precio_unidades, activo FROM LINEAVENTA WHERE id_producto = ? AND id_venta = ? AND activo = 1");
+			PreparedStatement ps = con.prepareStatement("SELECT ID_PRODUCTO, ID_VENTA, NUM_UNIDADES, PRECIO_UNIDADES FROM LINEAVENTA WHERE ID_PRODUCTO = ? AND ID_VENTA = ? FOR UPDATE");
 			ps.setInt(1, tlineaVenta.get_producto());
 			ps.setInt(2, tlineaVenta.get_venta());
 			
@@ -85,11 +83,10 @@ public class DAOLineaVentaImp implements DAOLineaVenta {
 			if (rs.next()) {
 				
 				tlineaventa = new TLineaVenta();
-				tlineaventa.set_producto(rs.getInt("id_producto"));
-				tlineaventa.set_venta(rs.getInt("id_venta"));
-				tlineaventa.set_num_unidades(rs.getInt("num_unidades"));
-				tlineaventa.set_precio_unidades(rs.getDouble("precio_unidades"));
-				tlineaventa.set_activo(rs.getInt("activo"));
+				tlineaventa.set_producto(rs.getInt("ID_PRODUCTO"));
+				tlineaventa.set_venta(rs.getInt("ID_VENTA"));
+				tlineaventa.set_num_unidades(rs.getInt("NUM_UNIDADES"));
+				tlineaventa.set_precio_unidades(rs.getDouble("PRECIO_UNIDADES"));
 			}
 			
 			rs.close();
@@ -116,7 +113,7 @@ public class DAOLineaVentaImp implements DAOLineaVenta {
 			Transaction tr = tm.getTransaction();
 			Connection con = (Connection)tr.getResource();
 			
-			PreparedStatement ps = con.prepareStatement("SELECT id_producto, id_venta, num_unidades, precio_unidades, activo FROM LINEAVENTA WHERE id_venta = ? FOR UPDATE");
+			PreparedStatement ps = con.prepareStatement("SELECT ID_PRODUCTO, ID_VENTA, NUM_UNIDADES, PRECIO_UNIDADES FROM LINEAVENTA WHERE ID_VENTA = ? FOR UPDATE");
 			ps.setInt(1, id_venta);
 			
 			java.sql.ResultSet rs = ps.executeQuery();
@@ -124,11 +121,10 @@ public class DAOLineaVentaImp implements DAOLineaVenta {
 			while (rs.next()) {
 				
 				TLineaVenta tlineaventa = new TLineaVenta();
-				tlineaventa.set_producto(rs.getInt("id_producto"));
-				tlineaventa.set_venta(rs.getInt("id_venta"));
-				tlineaventa.set_num_unidades(rs.getInt("num_unidades"));
-				tlineaventa.set_precio_unidades(rs.getDouble("precio_unidades"));
-				tlineaventa.set_activo(rs.getInt("activo"));
+				tlineaventa.set_producto(rs.getInt("ID_PRODUCTO"));
+				tlineaventa.set_venta(rs.getInt("ID_VENTA"));
+				tlineaventa.set_num_unidades(rs.getInt("NUM_UNIDADES"));
+				tlineaventa.set_precio_unidades(rs.getDouble("PRECIO_UNIDADES"));
 				
 				lineasVenta.add(tlineaventa);
 			}
@@ -156,12 +152,11 @@ public class DAOLineaVentaImp implements DAOLineaVenta {
 			Transaction tr = tm.getTransaction();
 			Connection con = (Connection)tr.getResource();
 			
-			PreparedStatement ps = con.prepareStatement("UPDATE LINEAVENTA SET num_unidades = ?, precio_unidades = ?, activo = ? WHERE id_producto = ? AND id_venta = ?");
+			PreparedStatement ps = con.prepareStatement("UPDATE LINEAVENTA SET NUM_UNIDADES = ?, PRECIO_UNIDADES = ? WHERE ID_PRODUCTO = ? AND ID_VENTA = ?");
 			ps.setInt(1, tlineaVenta.get_num_unidades());
 			ps.setDouble(2, tlineaVenta.get_precio_unidades());
-			ps.setInt(3, tlineaVenta.get_activo());
-			ps.setInt(4, tlineaVenta.get_producto());
-			ps.setInt(5, tlineaVenta.get_venta());
+			ps.setInt(3, tlineaVenta.get_producto());
+			ps.setInt(4, tlineaVenta.get_venta());
 			
 			int filas = ps.executeUpdate();
 			ps.close();
@@ -185,7 +180,7 @@ public class DAOLineaVentaImp implements DAOLineaVenta {
 			Transaction tr = tm.getTransaction();
 			Connection con = (Connection)tr.getResource();
 			
-			PreparedStatement ps = con.prepareStatement("SELECT id_producto, id_venta, num_unidades, precio_unidades, activo FROM LINEAVENTA WHERE id_producto = ? FOR UPDATE");
+			PreparedStatement ps = con.prepareStatement("SELECT ID_PRODUCTO, ID_VENTA, NUM_UNIDADES, PRECIO_UNIDADES FROM LINEAVENTA WHERE ID_PRODUCTO = ? FOR UPDATE");
 			ps.setInt(1, id_producto);
 			
 			java.sql.ResultSet rs = ps.executeQuery();
@@ -195,11 +190,10 @@ public class DAOLineaVentaImp implements DAOLineaVenta {
 			if (rs.next()) {
 				
 				tlineaventa = new TLineaVenta();
-				tlineaventa.set_producto(rs.getInt("id_producto"));
-				tlineaventa.set_venta(rs.getInt("id_venta"));
-				tlineaventa.set_num_unidades(rs.getInt("num_unidades"));
-				tlineaventa.set_precio_unidades(rs.getDouble("precio_unidades"));
-				tlineaventa.set_activo(rs.getInt("activo"));
+				tlineaventa.set_producto(rs.getInt("ID_PRODUCTO"));
+				tlineaventa.set_venta(rs.getInt("ID_VENTA"));
+				tlineaventa.set_num_unidades(rs.getInt("NUM_UNIDADES"));
+				tlineaventa.set_precio_unidades(rs.getDouble("PRECIO_UNIDADES"));
 			}
 			
 			rs.close();
