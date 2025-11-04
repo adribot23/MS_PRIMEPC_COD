e82848620979edbe9ee05d0e0c405aa0d5ed980b
AlvaroAdrada26 <aadrada@ucm.es>
2025-11-04 14:03:26 +0100

    SACliente espero que definitivo

--- PrimePcCode/src/negocio/Cliente/SAClienteImp.java
+++ PrimePcCode/src/negocio/Cliente/SAClienteImp.java
@@ -9,16 +9,10 @@ import java.util.Set;
 import integracion.Cliente.DAOCliente;
 import integracion.FactoriaDAO.DAOAbstractFactory;
 import negocio.Cliente.TCliente;
+import negocio.Proveedor.TProveedor;
 import integracion.Transaction.TManager;
 import integracion.Transaction.Transaction;
 
-/**
- * <!-- begin-UML-doc --> <!-- end-UML-doc -->
- * 
- * @author adria
- * @generated "UML a Java
- *            (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class SAClienteImp implements SACliente {
 
 	public SAClienteImp() {
@@ -28,41 +22,26 @@ public class SAClienteImp implements SACliente {
 	@Override
 	public int altaCliente(TCliente tCliente) {
 		int id = -1;
-
 		TManager m = TManager.getInstance();
 		Transaction tr = m.createTransaction();
-
+		DAOCliente daoCliente = DAOAbstractFactory.getInstancia().generaDAOCliente();
 		if (tr != null) {
 			tr.start();
-			DAOCliente daoCliente = DAOAbstractFactory.getInstancia().generaDAOCliente();
-
 			if (tCliente != null) {
 				TCliente leido = daoCliente.read_by_DNI(tCliente.getDni());
-
 				if (leido == null) {
 					id = daoCliente.create(tCliente);
-					if (id != -1)
-						tr.commit();
-					else
-						tr.rollback();
-
-				} else if (leido.getActivo() == 0 && leido.getClass().equals(tCliente.getClass())) {
+					tr.commit();
+				} else if (leido.getActivo() == 0) {
 					tCliente.setId(leido.getId());
-					int res = daoCliente.update(tCliente);
-					if (res != -1) {
-						id = tCliente.getId();
-						tr.commit();
-					} else
-						tr.rollback();
-
+					daoCliente.update(tCliente);
+					id = tCliente.getId();
+					tr.commit();
 				} else {
 					tr.rollback();
 				}
-			} else {
-				tr.rollback();
 			}
 		}
-
 		return id;
 	}
 
@@ -76,22 +55,19 @@ public class SAClienteImp implements SACliente {
 		if (tr != null) {
 			tr.start();
 			DAOCliente daoCliente = DAOAbstractFactory.getInstancia().generaDAOCliente();
+			TCliente cliente = daoCliente.read(id);
 
-			if (id >= 0) {
-				TCliente existente = daoCliente.read(id);
-
-				if (existente != null && existente.getActivo() == 1) {
+			if (cliente != null) {
+				if (cliente.getActivo() == 1) {
 					res = daoCliente.delete(id);
 					if (res != -1)
 						tr.commit();
 					else
 						tr.rollback();
-				} else {
+				} else
 					tr.rollback();
-				}
-			} else {
+			} else
 				tr.rollback();
-			}
 		}
 
 		return res;
@@ -106,77 +82,59 @@ public class SAClienteImp implements SACliente {
 
 		if (tr != null) {
 			tr.start();
+			
 			DAOCliente daoCliente = DAOAbstractFactory.getInstancia().generaDAOCliente();
+			
+			TCliente existente = daoCliente.read(cliente.getId());
 
-			if (cliente != null) {
-				TCliente existente = daoCliente.read(cliente.getId());
-
-				if (existente != null && existente.getActivo() == 1 && existente.getClass().equals(cliente.getClass())
-						&& (cliente.getDni().equals(existente.getDni())
-								|| daoCliente.read_by_DNI(cliente.getDni()) == null)) {
-
-					res = daoCliente.update(cliente);
-					if (res != -1)
+			if (existente != null && existente.getActivo() == 1 && (cliente.getNombre().equals(existente.getNombre())
+					|| daoCliente.read_by_DNI(cliente.getDni()) == null)) {
+				res = daoCliente.update(cliente);
+				if (res != -1)
+					if (res != -1) {
 						tr.commit();
-					else
+					} else {
 						tr.rollback();
+					}
 				} else {
 					tr.rollback();
 				}
-			} else {
-				tr.rollback();
 			}
-		}
-
-		return res;
+			return res;
 	}
 
 	@Override
 	public TCliente leerCliente(int id) {
-		TCliente leido = null;
-
 		TManager m = TManager.getInstance();
 		Transaction tr = m.createTransaction();
-
+		TCliente leido = null;
 		if (tr != null) {
 			tr.start();
 			DAOCliente daoCliente = DAOAbstractFactory.getInstancia().generaDAOCliente();
-
-			if (id >= 0) {
-				TCliente t = daoCliente.read(id);
-				if (t != null && t.getActivo() == 1) {
-					leido = t;
-					tr.commit();
-				} else {
-					tr.rollback();
-				}
-			} else {
+			leido = daoCliente.read(id);
+			if (leido != null && leido.getActivo() == 1)
+				tr.commit();
+			else {
 				tr.rollback();
 			}
-		}
 
+		}
 		return leido;
 	}
 
 	@Override
 	public Set<TCliente> leerTodosClientes() {
-		Set<TCliente> clientes = null;
-
 		TManager m = TManager.getInstance();
 		Transaction tr = m.createTransaction();
+		Set<TCliente> clientes = null;
 
 		if (tr != null) {
 			tr.start();
 			DAOCliente daoCliente = DAOAbstractFactory.getInstancia().generaDAOCliente();
-
-			clientes = new HashSet<>(daoCliente.read_all());
-			if (clientes != null) {
-				tr.commit();
-			} else {
-				tr.rollback();
-			}
+			clientes = daoCliente.read_all();
+			tr.commit();
 		}
-
+		
 		return clientes;
 	}
 }
