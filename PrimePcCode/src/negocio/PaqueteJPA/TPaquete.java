8c1835f45c90d3fc40cd2bc6b49a590d28088fcb
sertaran <sertaran@ucm.es>
2025-11-13 10:21:48 +0100

    TPaquete

--- PrimePcCode/src/negocio/PaqueteJPA/TPaquete.java
+++ PrimePcCode/src/negocio/PaqueteJPA/TPaquete.java
@@ -1,5 +1,44 @@
 package negocio.PaqueteJPA;
 
 public class TPaquete {
-
+	int id;
+	String estado;
+	double peso;
+	double precio;
+	
+	// getters
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public String getEstado() {
+		return this.estado;
+	}
+	
+	public double getPeso() {
+		return this.peso;
+	}
+	
+	public double getPrecio() {
+		return this.precio;
+	}
+	
+	// setters
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
 }
