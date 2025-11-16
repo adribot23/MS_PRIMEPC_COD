21ecb0df3ceec7acc2575e3b5033a55a1f450711
Daritessar <dariorom@ucm.es>
2025-11-16 13:03:30 +0100

    Creacion entity manager factory

---
+++ PrimePcCode/src/integracion/EMFSingleton/EMFSingleton.java
@@ -0,0 +1,17 @@
+package integracion.EMFSingleton;
+
+import javax.persistence.EntityManagerFactory;
+
+public abstract class EMFSingleton {
+	private static EMFSingleton instancia;
+
+	public static EMFSingleton getInstancia() {
+
+		if (instancia == null)
+			instancia = new EMFSingletonImp();
+
+		return instancia;
+	}
+
+	public abstract EntityManagerFactory getEntityManagerFactory();
+}
