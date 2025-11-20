554855d97928b8553575d20b6260852cc581a3d3
Mateo Gutierrez <mateogut@ucm.es>
2025-11-20 09:33:00 +0100

    Cambios Codigo TransporteJPA

--- PrimePcCode/src/negocio/TrabajadorJPA/SATrabajador.java
+++ PrimePcCode/src/negocio/TrabajadorJPA/SATrabajador.java
@@ -2,19 +2,16 @@ package negocio.TrabajadorJPA;
 
 import java.util.Set;
 
-import negocio.TrabajadorJPA.TTrabajador;
-
 public interface SATrabajador {
 
-	public int altaTrabajador(TTrabajador trabajador);
+	public int AltaTrabajador(TTrabajador trabajador);
+
+	public int BajaTrabajador(int id_trabajador);
+
+	public int ModificarTrabajador(TTrabajador trabajador);
+
+	public TTrabajador leerTrabajador(int id_trabajador);
 	
-
-	public int bajaTrabajador(int id_trabajador);
-
-	public int modificarTrabajador(TTrabajador trabajador);
-
-	public TTrabajador buscarTrabajador(int id_trabajador);
-
-	public Set<TTrabajador> mostrarTrabajador();
+	public Set<TTrabajador> leerTodosTrabajadores();
 	
 }
--- PrimePcCode/src/negocio/TrabajadorJPA/SATrabajadorImp.java
+++ PrimePcCode/src/negocio/TrabajadorJPA/SATrabajadorImp.java
@@ -10,34 +10,34 @@ import integracion.EMFSingleton.EMFSingleton;
 public class SATrabajadorImp implements SATrabajador{
 
 	@Override
-	public int altaTrabajador(TTrabajador trabajador) {
-		int id = -1;
-		EntityManager em = EMFSingleton.getInstancia().getEntityManagerFactory().createEntityManager();
-		EntityTransaction tr = em.getTransaction();
+	public int AltaTrabajador(TTrabajador trabajador) {
+	//	int id = -1;
+	//	EntityManager em = EMFSingleton.getInstancia().getEntityManagerFactory().createEntityManager();
+	//	EntityTransaction tr = em.getTransaction();
 		
 		return 0;
 	}
 
 	@Override
-	public int bajaTrabajador(int id_trabajador) {
+	public int BajaTrabajador(int id_trabajador) {
 		// TODO Apéndice de método generado automáticamente
 		return 0;
 	}
 
 	@Override
-	public int modificarTrabajador(TTrabajador trabajador) {
+	public int ModificarTrabajador(TTrabajador trabajador) {
 		// TODO Apéndice de método generado automáticamente
 		return 0;
 	}
 
 	@Override
-	public TTrabajador buscarTrabajador(int id_trabajador) {
+	public TTrabajador leerTrabajador(int id_trabajador) {
 		// TODO Apéndice de método generado automáticamente
 		return null;
 	}
 
 	@Override
-	public Set<TTrabajador> mostrarTrabajador() {
+	public Set<TTrabajador> leerTodosTrabajadores() {
 		// TODO Apéndice de método generado automáticamente
 		return null;
 	}
