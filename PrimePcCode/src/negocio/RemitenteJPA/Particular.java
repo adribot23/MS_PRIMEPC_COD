24ac351b37a6067bd019e1af9f38a6a25ee745fe
AlvaroAdrada26 <aadrada@ucm.es>
2025-11-22 18:03:54 +0100

    entidades Remitente implementadas

--- PrimePcCode/src/negocio/RemitenteJPA/Particular.java
+++ PrimePcCode/src/negocio/RemitenteJPA/Particular.java
@@ -1,5 +1,28 @@
 package negocio.RemitenteJPA;
 
-public class Particular {
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.NamedQuery;
+import javax.persistence.NamedQueries;
 
+@Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "Negocio.RemitenteJPA.Particular.findByfechaNacimiento",
+        query = "select p from Particular p where :fechaNacimiento = p.fechaNacimiento"
+    )
+})
+public class Particular extends Remitente implements Serializable {
+
+	private static final long serialVersionUID = 0;
+	
+	private String fechaNacimiento;
+	
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
 }
--- PrimePcCode/src/negocio/RemitenteJPA/Remitente.java
+++ PrimePcCode/src/negocio/RemitenteJPA/Remitente.java
@@ -1,5 +1,85 @@
 package negocio.RemitenteJPA;
 
-public class Remitente {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
 
+import java.io.Serializable;
+import javax.persistence.NamedQuery;
+import javax.persistence.Version;
+import javax.persistence.InheritanceType;
+
+
+import javax.persistence.NamedQueries;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@NamedQueries({
+    @NamedQuery(
+        name = "Negocio.RemitenteJPA.Remitente.findByid",
+        query = "select r from Remitente r where :id_remitente = r.id_remitente"
+    ),
+    @NamedQuery(
+        name = "Negocio.RemitenteJPA.Remitente.findBydireccion",
+        query = "select r from Remitente r where :direccion = r.direccion"
+    ),
+    @NamedQuery(
+        name = "Negocio.RemitenteJPA.Remitente.findBytelefono",
+        query = "select r from Remitente r where :telefono = r.telefono"
+    ),
+    @NamedQuery(
+        name = "Negocio.RemitenteJPA.Remitente.findByactivo",
+        query = "select r from Remitente r where :activo = r.activo"
+    ),
+    @NamedQuery(
+        name = "Negocio.RemitenteJPA.Remitente.findByversion",
+        query = "select r from Remitente r where :version = r.version"
+    )
+})
+public class Remitente implements Serializable{
+
+	private static final long serialVersionUID = 0;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+	private int id_remitente;
+    
+	private int activo;
+	private String direccion;
+	private String telefono;
+	
+	@Version
+	private Integer version;
+	
+	public Integer get_version() {
+		return version;
+	}
+	
+	public int getId() {
+		return id_remitente;
+	}
+	public void setId(int id_remitente) {
+		this.id_remitente = id_remitente;
+	}
+	public int getActivo() {
+		return activo;
+	}
+	public void setActivo(int activo) {
+		this.activo = activo;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
 }
