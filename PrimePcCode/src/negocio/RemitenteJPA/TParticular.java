0254663d6a9a809de43970e74181d57ed372ebbf
AlvaroAdrada26 <aadrada@ucm.es>
2025-11-22 17:51:31 +0100

    transfers implementados

--- PrimePcCode/src/negocio/RemitenteJPA/TParticular.java
+++ PrimePcCode/src/negocio/RemitenteJPA/TParticular.java
@@ -1,5 +1,19 @@
 package negocio.RemitenteJPA;
 
-public class TParticular {
+public class TParticular extends TRemitente{
 
+	public String fechaNacimiento;
+	
+	public TParticular(int id_remitente, int activo, String direccion, String telefono, String fechaNacimiento) {
+		super(id_remitente, activo, direccion, telefono);
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
 }
--- PrimePcCode/src/negocio/RemitenteJPA/TRemitente.java
+++ PrimePcCode/src/negocio/RemitenteJPA/TRemitente.java
@@ -2,4 +2,54 @@ package negocio.RemitenteJPA;
 
 public class TRemitente {
 
+	
+	private int id_remitente;
+	private int activo;
+	private String direccion;
+	private String telefono;
+	
+	public TRemitente(int id_remitente, int activo, String direccion, String telefono) {
+		super();
+		this.id_remitente = id_remitente;
+		this.activo = activo;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+	
+	public TRemitente(Remitente remitente) {
+		this.id_remitente = remitente.getId();
+		this.activo = remitente.getActivo();
+		this.direccion = remitente.getDireccion();
+		this.telefono = remitente.getTelefono();
+	}
+	
+	public int getId() {
+		return id_remitente;
+	}
+	public void setId(int id_remitente) {
+		this.id_remitente = id_remitente;
+	}
+	public int getActivo() {
+		return activo;
+	}
+	public void setActivo(int activo) {
+		this.activo = activo;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public TRemitente() {
+		
+	}
+	
 }
