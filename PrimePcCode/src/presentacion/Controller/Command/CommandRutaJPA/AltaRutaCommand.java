83ca86831b8df7021ab6c2433337cdc9253255ee
jorgecafe <jorgecalvof@pm.me>
2025-11-29 11:17:50 +0100

    AltaRutaCommand

---
+++ PrimePcCode/src/presentacion/Controller/Command/CommandRutaJPA/AltaRutaCommand.java
@@ -0,0 +1,19 @@
+package presentacion.Controller.Command.CommandRutaJPA;
+
+import negocio.FactoriaSA.SAAbstractFactory;
+import negocio.RutaJPA.TRuta;
+import presentacion.Controller.Command.Command;
+import presentacion.Controller.Command.Context;
+import presentacion.GUI.Evento;
+
+public class AltaRutaCommand implements Command {
+
+	@Override
+	public Context execute(Object data) {
+		int res = SAAbstractFactory.getInstancia().generarSARuta().altaRuta((TRuta) data);
+		if (res > 0)
+			return new Context(Evento.RES_ALTA_RUTA_OK, res);
+		else
+			return new Context(Evento.RES_ALTA_RUTA_KO, null);
+	}
+}
