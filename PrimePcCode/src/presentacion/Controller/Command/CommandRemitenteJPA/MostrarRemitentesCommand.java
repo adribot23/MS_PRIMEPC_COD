bb0f93b8635cc7faa3c3dc6e5c307bb39b581c7b
AlvaroAdrada26 <aadrada@ucm.es>
2025-11-30 01:05:30 +0100

    comandos remitente hechos

--- PrimePcCode/src/presentacion/Controller/Command/CommandRemitenteJPA/MostrarRemitentesCommand.java
+++ PrimePcCode/src/presentacion/Controller/Command/CommandRemitenteJPA/MostrarRemitentesCommand.java
@@ -1,5 +1,23 @@
 package presentacion.Controller.Command.CommandRemitenteJPA;
 
-public class MostrarRemitentesCommand {
+import presentacion.Controller.Command.Command;
+import presentacion.Controller.Command.Context;
+import presentacion.GUI.Evento;
 
+import java.util.Set;
+
+import negocio.FactoriaSA.SAAbstractFactory;
+import negocio.RemitenteJPA.TRemitente;
+
+public class MostrarRemitentesCommand implements Command {
+
+	public Context execute(Object data) {
+
+		Set<TRemitente> remitentes = SAAbstractFactory.getInstancia().generarSARemitente().listarTodosRemitentes();
+
+		if (remitentes != null && !remitentes.isEmpty())
+			return new Context(Evento.RES_MOSTRAR_TODOS_REMITENTES_OK, remitentes);
+		else
+			return new Context(Evento.RES_MOSTRAR_TODOS_REMITENTES_KO, remitentes);
+	}
 }
