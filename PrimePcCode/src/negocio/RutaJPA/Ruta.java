533845e9bf5ce313f777497ac2cde1c05fa731c4
adrizz8 <adriro11@ucm.es>
2025-12-01 17:40:14 +0100

    Entidad ruta a falta de etiquetas de relacion y Tranfer de Ruta

---
+++ PrimePcCode/src/negocio/RutaJPA/Ruta.java
@@ -0,0 +1,95 @@
+package negocio.RutaJPA;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Ruta {
+	private static final long serialVersionUID = 0;
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private int id_ruta;
+
+	@Version
+	private Integer version;
+	
+	private String origen;
+	private String destino;
+	private double distancia;
+	private int activo;
+	
+	
+	
+	
+
+	
+	
+	public Ruta() {}
+	
+	public Ruta(TRuta ruta) {
+		
+		this.id_ruta = ruta.get_id();
+		this.origen = ruta.get_origen();
+		this.destino = ruta.get_destino();
+		this.distancia = ruta.get_distancia();
+		this.activo = ruta.get_activo();
+	}
+	
+	public TRuta entityToTransfer() {
+		TRuta tRuta = new TRuta();
+		tRuta.set_id(this.id_ruta);
+		tRuta.set_origen(this.origen);
+		tRuta.set_destino(this.destino);
+		tRuta.set_distancia(this.distancia);
+		tRuta.set_activo(this.activo);
+		return tRuta;
+	}
+	
+	
+	public int getId() {
+		return this.id_ruta;
+	}
+	
+	public String getOrigen() {
+		return this.origen;
+	}
+	
+	public String getDestino() {
+		return this.destino;
+	}
+	
+	public double getDistancia() {
+		return this.distancia;
+	}
+	
+	
+	public int getActivo() {
+		return this.activo;
+	}
+	
+
+	public void setId(int id) {
+		this.id_ruta = id;
+	}
+	
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+	
+	public void setDistancia(double distancia) {
+		this.distancia = distancia;
+	}
+	
+	public void setActivo(int activo) {
+		this.activo = activo;
+	}
+	
+}
