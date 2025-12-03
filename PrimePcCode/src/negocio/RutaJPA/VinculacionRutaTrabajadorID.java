2cb72078742c15885f7e8b1e537ed2a80897dfe1
adrizz8 <adriro11@ucm.es>
2025-12-03 21:47:22 +0100

    Entidad de vinculacion y su ID

---
+++ PrimePcCode/src/negocio/RutaJPA/VinculacionRutaTrabajadorID.java
@@ -0,0 +1,57 @@
+package negocio.RutaJPA;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class VinculacionRutaTrabajadorID implements Serializable {
+
+	private static final long serialVersionUID = 0;
+
+	int ruta;
+	int trabajador;
+
+	public VinculacionRutaTrabajadorID() {
+	}
+
+	public VinculacionRutaTrabajadorID(int ruta, int trabajador) {
+		this.ruta = ruta;
+		this.trabajador = trabajador;
+	}
+
+	int get_ruta() {
+		return this.ruta;
+	}
+
+	int get_trabajador() {
+		return this.trabajador;
+	}
+
+	void set_ruta(int ruta) {
+		this.ruta = ruta;
+	}
+
+	void set_trabajador(int trabajador) {
+		this.trabajador = trabajador;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ruta, trabajador);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VinculacionRutaTrabajadorID other = (VinculacionRutaTrabajadorID) obj;
+		return ruta == other.ruta && trabajador == other.trabajador;
+	}
+
+}
