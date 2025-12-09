8088bcda4e2285d902e6600433a5db0a6bdcf5a2
SerBarbas <sbarbas@ucm.es>
2025-12-09 16:58:04 +0100

    Mostrar venta por remitente
    Co-Authored-By: ritamald <187155040+ritamald@users.noreply.github.com>

--- PrimePcCode/src/presentacion/FacturaJPA/VListarFacturasPorRemitente.java
+++ PrimePcCode/src/presentacion/FacturaJPA/VListarFacturasPorRemitente.java
@@ -74,7 +74,7 @@ public class VListarFacturasPorRemitente extends JFrame implements IGUI{
 	}
 
 	private void mostrarTabla(Set<TFactura> facturas) {
-		String[] columnNames = { "ID Factura", "ID Remitente", "Precio Bruto", "Precio Neto" }; //hace falta num paquetes?
+		String[] columnNames = { "ID Factura", "ID Remitente", "Precio Total", "Activo" }; 
 		Object[][] tableData = new Object[facturas.size()][columnNames.length];
 
 		int i = 0;
@@ -82,6 +82,7 @@ public class VListarFacturasPorRemitente extends JFrame implements IGUI{
 			tableData[i][0] = f.get_idFactura();
 			tableData[i][1] = f.get_idRemitente();
 			tableData[i][2] = f.get_precioTotal();
+			tableData[i][3] = f.get_activo();
 			i++;
 		}
 
