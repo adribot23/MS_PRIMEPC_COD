52f98752015601b717b55d20c423a6e6a9a48a65
Mateo Gutierrez <mateogut@ucm.es>
2025-12-10 11:49:36 +0100

    Ultimisisismos detalles TrabjadorJPA y RutaJPA

--- PrimePcCode/src/presentacion/Controller/Command/CommandRutaJPA/VerTrabajadoresPorRutaCommand.java
+++ PrimePcCode/src/presentacion/Controller/Command/CommandRutaJPA/VerTrabajadoresPorRutaCommand.java
@@ -17,19 +17,9 @@ public class VerTrabajadoresPorRutaCommand implements Command {
 		Set<TVinculacionRutaTrabajador> vinculaciones = SAAbstractFactory.getInstancia()
 				.generarSAVinculacionRutaTrabajador().listar_vinculaciones_por_ruta((int) data);
 
-		Set<TTrabajador> trabajadores = new LinkedHashSet<>();
-		if (vinculaciones != null) {
-			for (TVinculacionRutaTrabajador vinc : vinculaciones) {
-				TTrabajador t = SAAbstractFactory.getInstancia().generarSATrabajador()
-						.leerTrabajador(vinc.get_id_trabajador());
-				if (t != null) {
-					trabajadores.add(t);
-				}
-			}
-		}
 
-		if (!trabajadores.isEmpty()) {
-			return new Context(Evento.RES_VER_TRABAJADOR_POR_RUTA_OK, trabajadores);
+		if (!vinculaciones.isEmpty()) {
+			return new Context(Evento.RES_VER_TRABAJADOR_POR_RUTA_OK, vinculaciones);
 		} else {
 			return new Context(Evento.RES_VER_TRABAJADOR_POR_RUTA_KO, null);
 		}
